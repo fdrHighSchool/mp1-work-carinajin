4ba965dfe23134d08b16b6cf5d99124d29c71ab8
AP Java Period 5 <apjavaperiod5@ac1mwk0lbjc9h.78b505.nycboe.org>
2022-10-04 12:14:48 -0400

    added favorite movies

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FavoriteMovie
+{
+    public static void main(String[]args) {
+        System.out.println("My favorite movie is Ponyo by Studio Ghibli.");
+    }
+}
