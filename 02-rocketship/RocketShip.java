7fd96bbab0fe12f126f9bef2d5948ba1f113fcec
AP Java Period 5 <apjavaperiod5@ac1mwk0lbjc9h.78b505.nycboe.org>
2022-10-25 13:28:12 -0400

    catch up

---
+++ 02-rocketship/RocketShip.java
@@ -0,0 +1,28 @@
+// Authors: Andy Huynh and Carina Jin
+public class RocketShip
+{
+    public static void main(String[] args) {
+        triangle();
+        box();
+        US();
+        box();
+        triangle();
+ }//end of main
+ public static void triangle() {
+     System.out.println("   /\\  ");
+     System.out.println("  /  \\ ");
+     System.out.println(" /    \\");
+    }//end of triangle
+ public static void box() {
+     System.out.println("+------+");
+     System.out.println("|      |");
+     System.out.println("|      |");
+     System.out.println("+------+");
+    }//end of box
+ public static void US() {
+     System.out.println("|United|");
+     System.out.println("|States|");
+ }//end of US
+
+     
+    }
--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -4,15 +4,22 @@ public class UserName {
   public static void main(String[] args) {
     // create Scanner object
     Scanner s = new Scanner(System.in);
-
+    
     // get String input from user
+    System.out.print("Are you a student or teacher? ");
+    String role = s.nextLine();
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
     System.out.print("Enter your last name: ");
     String lastName = s.nextLine();
-
+    System.out.print("Enter your favorite number: ");
+    String favNum = s.nextLine();
+    if (role.equals("student")) {
+        System.out.print("");
+    }
+    
     // test output
-    System.out.println("Hello " + initialize(firstName) + "." + initialize(lastName) + ".");
+    System.out.println("Hello " + initialize(firstName) + "." + initialize(lastName) + ". " + favNum);
 
     s.close();
   } // end main method
@@ -26,5 +33,9 @@ public class UserName {
   public static String initialize(String n) {
     return n.substring(0, 1);
   } // end initialize method
+  public static String generatePassword(int length) {
+      String password;
+      return password;
+  }
 
 } // end class
